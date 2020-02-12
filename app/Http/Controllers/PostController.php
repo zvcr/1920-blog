18f563514f39f457fbcded5df652593894b65c9d
D. Bergmans <d.bergmans@rocwb.nl>
2020-02-12 14:15:02 +0100

    Lesson 200210 Added post overview

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,5 +8,9 @@ use App\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::all();
+        // dd($posts);
+        return view("post.index")->withPosts($posts);
+    }
 }
---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1 class="title">Lees hier al onze berichten</h1>
+                <div class="posts">
+                @foreach($posts as $post)
+                <div class="post">
+                    <p class="post-title">Title: <a href="#">{{ $post->title }}</a></p><br/>
+                    <i>Geplaatst op: {{ $post->published_at ?? "Nog niet gepubliseerd" }}</i>
+                </div>
+                @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
