2b9a83459ca1586edadca165d64d1e731560e1ac
D. Bergmans <d.bergmans@rocwb.nl>
2020-02-18 14:04:22 +0100

    Lesson 200218 - Minor style changes

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <h1 class="title">Lees hier al onze berichten</h1>
                 <div class="posts">
                 @foreach($posts as $post)
-                <div class="post">
-                    <h2 class="post-title"><a href="/post/{{ $post->slug }}">{{ $post->title }}</a></h2><br/>
-                    <i>Geplaatst op: {{ $post->published_at ?? "Nog niet gepubliseerd" }}</i>
+                <hr/>
+                <div class="row post">
+                    <div class="col-md-9">
+                        <h2 class="post-title"><a href="/post/{{ $post->slug }}">{{ $post->title }}</a></h2>
+                    </div>
+                    <div class="col-md-3">
+                        <i class="post-published-at">Geplaatst op: {{ $post->published_at ?? "Nog niet gepubliseerd" }}</i>
+                    </div>
                 </div>
                 @endforeach
                 </div>
